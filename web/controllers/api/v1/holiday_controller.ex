c8668ee44196c8fb12a562aeb9c96eb89a2bab5a
Aleksei Matiushkin <aleksei.matiushkin@kantox.com>
2015-12-14 19:07:43 +0100

    Agent works. It’s necessary to add error handling for inexisting currencies.

--- web/controllers/api/v1/holiday_controller.ex
+++ web/controllers/api/v1/holiday_controller.ex
@@ -5,40 +5,49 @@ defmodule MuResponse.Api.V1.HolidayController do
   use MuResponse.Web, :controller
 
   def index(conn, _params) do
-    {:ok, files} = File.cwd!
-                   |> Path.join("data/holidays")
-                   |> File.ls
-    links = Enum.map(files, fn(file) -> api_v1_holiday_url(Endpoint, :show, String.slice(file, 0..2)) end)
-    json conn, %{ currencies: links }
+    json conn, %{ currencies: Enum.map(get_currency_list, fn file ->
+      api_v1_holiday_url(Endpoint, :show, file)
+    end) }
   end
 
   def show(conn, %{"id" => id}) do
-    holidays = get_currency(id)
-    json conn, %{ holidays: %{ currency: id, days: holidays } }
+    json conn, %{ holidays: %{ currency: id, days: get_currency(id) } }
   end
 
   #########################################
   ############## persistence ##############
   #########################################
+  def start_link do
+    case Agent.start_link(fn -> %{} end, name: __MODULE__) do
+      {:ok, pid} ->
+        Enum.each(get_currency_list, fn(currency) -> load_currency(currency) end)
+        {:ok, pid}
+      _ -> :error
+    end
+  end
+
+  def get_currency_list do
+    case File.cwd! |> Path.join("data/holidays") |> File.ls do
+      {:ok, files} ->
+        Enum.map(files, fn file -> String.slice(file, 0..2) end)
+      _ -> :error
+    end
+  end
+
   def get_currency(currency) do
     Agent.get(__MODULE__, fn map ->
       case Map.fetch(map, currency) do
         {:ok, value} -> value
-        :error -> load_currency(currency)
+        :error -> []
       end
     end)
-  end
-
-  def start_link do
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
   def load_currency(currency) do
     holidays = File.cwd!
                |> Path.join("data/holidays/#{currency}.yml")
                |> YamlElixir.read_from_file
-    # Agent.update(__MODULE__, &Map.put(&1, currency, holidays))
-    holidays
+    Agent.update(__MODULE__, &Map.put(&1, currency, holidays))
   end
 
 end
